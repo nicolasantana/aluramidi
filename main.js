dd944cecf2745a69f0aa9041d9fed3658248ad22
nicolasantana <127311790+nicolasantana@users.noreply.github.com>
2023-10-20 16:34:06 -0300

    s

--- main.js
+++ main.js
@@ -1,1 +1,3 @@
-document.querySelector('som_tecla_pom').play().play
+document.querySelector('som_tecla_pom').play().play;
+document.querySelector('tecla_pom');
+document.querySelector('som_tecla_pom').play();
